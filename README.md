acd43a039013c13bc15f2da99dc228959bc37554
Baptiste Pillot <baptiste@pillot.fr>
2025-03-26 15:58:35 +0100

    v0.0.1

---
+++ README.md
@@ -0,0 +1,7 @@
+[![npm version](https://img.shields.io/npm/v/@itrocks/config?logo=npm)](https://www.npmjs.org/package/@itrocks/config)
+[![npm downloads](https://img.shields.io/npm/dm/@itrocks/config)](https://www.npmjs.org/package/@itrocks/config)
+[![GitHub](https://img.shields.io/github/last-commit/itrocks-ts/config?color=2dba4e&label=commit&logo=github)](https://github.com/itrocks-ts/config)
+[![issues](https://img.shields.io/github/issues/itrocks-ts/config)](https://github.com/itrocks-ts/config/issues)
+[![discord](https://img.shields.io/discord/1314141024020467782?color=7289da&label=discord&logo=discord&logoColor=white)](https://25.re/ditr)
+
+# config
---
+++ src/config.ts
@@ -0,0 +1,85 @@
+import { appDir }   from '@itrocks/app-dir'
+import { readdir }  from 'node:fs/promises'
+import { readFile } from 'node:fs/promises'
+import { dirname }  from 'node:path'
+import { join }     from 'node:path'
+import { parse }    from 'yaml'
+
+export type Config = Record<string, any>
+
+export const config: Config = {}
+
+async function loadDir(path: string, relativePathSlice = 0)
+{
+	await loadFile(join(path, 'config.yaml'), relativePathSlice)
+	await loadFile(join(path, 'local.yaml'), relativePathSlice)
+}
+
+async function loadFile(file: string, relativePathSlice = 0)
+{
+	let yaml: string
+	try {
+		yaml = await readFile(file, 'utf-8')
+	}
+	catch {
+		return
+	}
+	loadYaml(yaml, relativePathSlice ? dirname(file).slice(relativePathSlice) : dirname(file))
+}
+
+function loadYaml(buffer: string, path: string)
+{
+	mergeConfig(config, parse(buffer), path)
+}
+
+function mergeConfig(config: Config, merge: Config, path: string)
+{
+	Object.entries(merge).forEach(([key, value]) =>
+	{
+		const pathValue = (value: string) => ((typeof value === 'string') && value.startsWith('./'))
+			? (path + value.slice(1))
+			: value
+		if (!(key in config)) {
+			config[key] = pathValue(value)
+			return
+		}
+		if (Array.isArray(value)) {
+			if (!Array.isArray(config[key])) {
+				config[key] = [config[key]]
+			}
+			config[key].push(...value.map(pathValue))
+			return
+		}
+		if (Array.isArray(config[key])) {
+			config[key].push(pathValue(value))
+			return
+		}
+		if (typeof config[key] === 'object') {
+			mergeConfig(config[key], value, path)
+			return
+		}
+		config[key] = pathValue(value)
+	})
+}
+
+async function scanModules(path: string, relativePathSlice = 0)
+{
+	const entries = await readdir(path, { withFileTypes: true })
+	await Promise.all(entries.map(async entry => {
+		if (!entry.isDirectory()) {
+			return
+		}
+		if (entry.name[0] === '@') {
+			await scanModules(join(path, entry.name), relativePathSlice)
+			return
+		}
+		await loadDir(join(path, entry.name), relativePathSlice)
+	}))
+}
+
+export async function scanConfigFiles(path = appDir)
+{
+	const relativePathSlice = path.startsWith(appDir) ? appDir.length : 0
+	await scanModules(join(path, 'node_modules'), relativePathSlice)
+	await loadDir(path, relativePathSlice)
+}
